ce1bb316873d3952d907c27a0839f61f26b5f659
Roger Hutchings <roger@incuna.com>
2014-06-05 14:19:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# img2iss
+
+Turns a directory of images into GitHub issues for a given repo, using Imgur to store the images.
+
+## Getting Started
+Install the module with: `npm install img2iss`
+
+```javascript
+var img2iss = require('img2iss');
+img2iss.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 Roger Hutchings  
+Licensed under the GPL-2.0 license.
---
+++ test/img2iss_test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var img2iss = require('../lib/img2iss.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['app'] = function (test) {
+    
+    test.expect(1);
+    
+    // tests here
+    test.equal(img2iss.app(), 'awesome', 'should be awesome.');
+    test.done();
+
+};
