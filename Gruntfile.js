91a6cee2cbdf662bd2bcc9b1f338b4ed5da74900
Roger Hutchings <roger@incuna.com>
2014-06-05 17:42:49 +0100

    Refactor, add Grunt, prepare for tests!

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+(function() {
+
+    'use strict';
+
+    module.exports = function (grunt) {
+
+        require('jit-grunt')(grunt);
+        require('time-grunt')(grunt);
+
+        // Project configuration.
+        grunt.initConfig({
+            
+            nodeunit: {
+                files: ['test/**/*_test.js'],
+            },
+            
+            jshint: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                gruntfile: {
+                    src: 'Gruntfile.js'
+                },
+                lib: {
+                    src: ['lib/**/*.js']
+                },
+                test: {
+                    src: ['test/**/*.js']
+                },
+            },
+            
+            watch: {
+                jshint: {
+                    files: '<%= jshint.options.jshintrc %>',
+                    tasks: ['jshint']
+                },
+                gruntfile: {
+                    files: '<%= jshint.gruntfile.src %>',
+                    tasks: ['jshint:gruntfile']
+                },
+                lib: {
+                    files: '<%= jshint.lib.src %>',
+                    tasks: ['jshint:lib', 'nodeunit']
+                },
+                test: {
+                    files: '<%= jshint.test.src %>',
+                    tasks: ['jshint:test', 'nodeunit']
+                },
+            },
+        });
+
+        // Default task.
+        grunt.registerTask('default', 'watch');
+
+    };
+
+}());
